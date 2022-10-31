cea7678c4e02c24300428d7462a25a25e5fbbd50
michaeliu <michaeliu718@gmail.com>
2022-10-31 11:33:44 -0600

    JUnit set up

---
+++ code/app/src/test/java/com/example/cookingapp/IngredientTest.java
@@ -0,0 +1,8 @@
+package com.example.cookingapp;
+
+public class IngredientTest {
+    private Ingredient mockIngredient(){
+        Ingredient ingredient = new Ingredient();
+        return ingredient;
+    }
+}
---
+++ code/app/src/test/java/com/example/cookingapp/RecipeTest.java
@@ -0,0 +1,8 @@
+package com.example.cookingapp;
+
+public class RecipeTest {
+    private Recipe mockRecipe() {
+        Recipe recipe = new Recipe();
+        return recipe;
+    }
+}
