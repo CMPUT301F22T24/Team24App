450c1ab0fe961487cda68bccb7fae203ee2f89eb
dennea <dennea1228@gmail.com>
2022-11-01 16:56:33 -0600

    recipe tests

--- code/app/src/test/java/com/example/cookingapp/RecipeTest.java
+++ code/app/src/test/java/com/example/cookingapp/RecipeTest.java
@@ -1,8 +1,70 @@
 package com.example.cookingapp;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+
 public class RecipeTest {
     private Recipe mockRecipe() {
-        Recipe recipe = new Recipe();
-        return recipe;
+        return new Recipe();
     }
+
+    private Ingredient mockIngredient() {
+        return new Ingredient();
+    }
+
+    @Test
+    public void titleTest(){
+        Recipe recipe = mockRecipe();
+        recipe.setTitle("Tacos");
+        assertEquals("Tacos",recipe.getTitle());
+    }
+
+    @Test
+    public void servingsTest(){
+        Recipe recipe = mockRecipe();
+        recipe.setServings("5");
+        assertEquals("5",recipe.getServings());
+    }
+
+    @Test
+    public void categoryTest(){
+        Recipe recipe = mockRecipe();
+        recipe.setCategory("Dinner");
+        assertEquals("Dinner",recipe.getCategory());
+    }
+
+    @Test
+    public void commentsTest(){
+        Recipe recipe = mockRecipe();
+        recipe.setComments("comment");
+        assertEquals("comment", recipe.getComments());
+    }
+
+    @Test
+    public void prepTimeTest(){
+        Recipe recipe = mockRecipe();
+        recipe.setPrepTime("5 hours 10 min");
+        assertEquals("5 hours 10 min", recipe.getPrepTime());
+    }
+
+    @Test
+    public void imageTest(){
+        Recipe recipe = mockRecipe();
+        recipe.setImage("image");
+        assertEquals("image", recipe.getImage());
+    }
+
+    @Test
+    public void ingredientTest(){
+        Recipe recipe = mockRecipe();
+        Ingredient ingredient = mockIngredient();
+        ArrayList<Ingredient> ingredients = new ArrayList<>();
+        ingredients.add(ingredient);
+        recipe.setIngredients(ingredients);
+        assertEquals(ingredients, recipe.getIngredients());
+    }
+
 }
