66cb88dac71dbe808973ac25ca2f35a060e67c25
michaeliu <michaeliu718@gmail.com>
2022-11-01 20:49:24 -0600

    Add basic IngredientTest

--- code/app/src/test/java/com/example/cookingapp/IngredientTest.java
+++ code/app/src/test/java/com/example/cookingapp/IngredientTest.java
@@ -1,8 +1,57 @@
 package com.example.cookingapp;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 public class IngredientTest {
     private Ingredient mockIngredient(){
         Ingredient ingredient = new Ingredient();
         return ingredient;
+    }
+
+    @Test
+    public void DescriptionTest() {
+        Ingredient ingredient = mockIngredient();
+        ingredient.setDescription("food");
+        assertEquals("food", ingredient.getDescription());
+    }
+
+    @Test
+    public void LocationTest() {
+        Ingredient ingredient = mockIngredient();
+        ingredient.setLocation("fridge");
+        assertEquals("fridge", ingredient.getLocation());
+    }
+
+    @Test
+    public void AmountTest() {
+        Ingredient ingredient = mockIngredient();
+        ingredient.setAmount(1.0);
+        String amount = String.valueOf(ingredient.getAmount());
+        assertEquals("1.0", amount);
+    }
+
+    @Test
+    public void DocumentIdTest() {
+        Ingredient ingredient = mockIngredient();
+        ingredient.setDocumentId("123");
+        assertEquals("123", ingredient.getDocumentId());
+    }
+
+    @Test
+    public void UnitTest() {
+        Ingredient ingredient = mockIngredient();
+        ingredient.setUnit("kg");
+        assertEquals("kg", ingredient.getUnit());
+    }
+
+    @Test
+    public void Category() {
+        Ingredient ingredient = mockIngredient();
+        ingredient.setCategory("Fruits");
+        assertEquals("Fruits", ingredient.getCategory());
     }
 }
