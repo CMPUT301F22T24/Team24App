1fa53009e27f0c204b68fe017b0d3cae64a8825e
kooner27 <prabhnoo@ualberta.ca>
2022-11-23 23:31:21 -0700

    populated list for add recipe ingredient storage selection

---
+++ code/app/src/main/java/com/example/cookingapp/SelectFromStorage.java
@@ -0,0 +1,13 @@
+package com.example.cookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectFromStorage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
